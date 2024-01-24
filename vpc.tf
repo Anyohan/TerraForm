b851d71bd734e114f9d2cf95daac3fd4de5759d0
Anyohan <dygks3240@hanyang.ac.kr>
2024-01-24 14:42:26 +0900

    [ADD] AWS 리소스 정의
    인스턴스 및 VPC, 보안그룹 설정

---
+++ vpc.tf
@@ -0,0 +1,24 @@
+module "my-vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = var.vpc_az
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = false
+  enable_vpn_gateway = false
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
+variable "vpc_az" {
+  description = "az in seoul region"
+  type = list(string)
+  default = ["ap-northeast-2a","ap-northeast-2b","ap-northeast-2c"]
+}
